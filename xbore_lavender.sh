03272f13205d445c4e4bf2fbc2880694d1bfe6a3
Jody Yuantoro <i.xyzuan@gmail.com>
2021-06-28 14:39:07 +0700

    Update xbore_lavender.sh

--- xbore_lavender.sh
+++ xbore_lavender.sh
@@ -1,90 +1,62 @@
 #!/usr/bin/env bash
-echo "Downloading few Dependecies . . ."
-git clone --depth=1 https://github.com/kdrag0n/proton-clang proton
-
 # Main
-KERNEL_ROOTDIR=$(pwd)/lavender # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG=lavender-perf_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=$(pwd)/proton # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=xyzuan # Change with your own name or else.
 export KBUILD_BUILD_HOST=xyzscape-clouddroneci # Change with your own hostname.
-IMAGE=$(pwd)/lavender/out/arch/arm64/boot/Image.gz-dtb
+IMAGE=$(pwd)/xdroid/out/target/product/lavender/xdroid*.zip
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
 
-# Checking environtment
-# Warning !! Dont Change anything there without known reason.
-function check() {
-echo ================================================
-echo xKernelCompiler
-echo version : rev1.5 - gaspoll
-echo ================================================
-echo BUILDER NAME = ${KBUILD_BUILD_USER}
-echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
-echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
-echo CLANG_VERSION = $(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
-echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
-echo ================================================
+# Private CI
+curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="${chat_id}" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>xRomBuilder<b/>jajal"
+
+# Working Dir
+mkdir xdroid
+cd $(pwd)/xdroid
+
+# Sync Sources
+function sync() {
+repo init -u https://github.com/xdroid-CAF/xdroid_manifest -b eleven
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+}
+
+# Clone Device Stuff
+function clonedevicedep() {
+git clone https://github.com/xyz-prjkt/xdroid_device_lavender device/xiaomi/lavender --depth=1 -b -b eleven-caf
+git clone https://github.com/xyz-prjkt/xbore_strmbreakr_lavender kernel/xiaomi/lavender -b eleven-caf_eas
+git clone https://github.com/xyz-prjkt/hw_media hardware/qcom-caf/msm8998/media -b 11-LA.UM.9.6.2.r1-03600-89xx.0
+git clone https://github.com/xyz-prjkt/hw_audio hardware/qcom-caf/msm8998/audio -b 11-LA.UM.9.6.2.r1-03600-89xx.0
+git clone https://github.com/xyz-prjkt/hw_display hardware/qcom-caf/msm8998/display -b 11-LA.UM.9.6.2.r1-03600-89xx.0
 }
 
 # Compiler
 function compile() {
-
-   # Private CI
-   curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
-        -d chat_id="${chat_id}" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
-
-   # xyzplaygrnd
-   curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
-        -d chat_id="-1001389519102" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG VERSION : <code>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
-
-  cd ${KERNEL_ROOTDIR}
-  make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-  make -j$(nproc) ARCH=arm64 O=out \
-	CC=${CLANG_ROOTDIR}/bin/clang \
-	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+	
+	. build/envsetup.sh
+	lunch xdroid_lavender-userdebug
+	make carthage -j$(nproc)
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
 	exit 1
    fi
         git clone --depth=1 https://github.com/xyz-prjkt/AnyKernel3 AnyKernel
-	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-}
-
-
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot${token}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAECfcRg2RoccdYCRdKV9VvHTsGGzPfAGwACSwYAAio_yVZ1PSnOxIKyfSAE-aN927wS5blhsE" \
-        -d chat_id="${chat_id}"
-}
-
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot${token}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAECfcRg2RoccdYCRdKV9VvHTsGGzPfAGwACSwYAAio_yVZ1PSnOxIKyfSAE-aN927wS5blhsE" \
-        -d chat_id="-1001389519102"
+	mv $(pwd)/xdroid/out/target/product/lavender/xdroid*.zip $(pwd)
 }
 
 # Push kernel to channel
 function push() {
-    cd AnyKernel
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot${token}/sendDocument" \
+    cd $(pwd)
+    curl -F document=xdroid*.zip "https://api.telegram.org/bot${token}/sendDocument" \
         -F chat_id="${chat_id}" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
         -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7 (lavender)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 
-    curl -F document=@$ZIP "https://api.telegram.org/bot${token}/sendDocument" \
+    curl -F document=xdroid*.zip "https://api.telegram.org/bot${token}/sendDocument" \
         -F chat_id="-1001389519102" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
@@ -108,15 +80,10 @@ function finerr() {
     exit 1
 }
 
-# Zipping
-function zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 xBoreUp.strmbreakr-lavender-${DATE}.zip *
-    cd ..
 }
-check
+sync
+clonedevicedep
 compile
-zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
 push
