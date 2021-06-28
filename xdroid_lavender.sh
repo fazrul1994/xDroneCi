f06998cfa538a56d7b040b809529055d3b3b1f50
Jody Yuantoro <i.xyzuan@gmail.com>
2021-06-28 14:57:29 +0700

    Update xdroid_lavender.sh

--- xdroid_lavender.sh
+++ xdroid_lavender.sh
@@ -7,6 +7,12 @@ DATE=$(date +"%F-%S")
 START=$(date +"%s")
 
 # Installing some dependency
+sudo apt install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison \
+build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev \
+lib32readline-dev lib32z1-dev  liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
+libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev \
+libtinfo5 libncurses5
+
 mkdir $(pwd)/bin
 PATH=$(pwd)/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > $(pwd)/bin/repo
