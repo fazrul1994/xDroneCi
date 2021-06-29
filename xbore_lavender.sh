e24fbffd4500286cd6e41b3affd6007668dc4fa8
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-06-29 13:37:50 +0700

    Update xbore_lavender.sh

--- xbore_lavender.sh
+++ xbore_lavender.sh
@@ -47,6 +47,7 @@ function compile() {
 
   cd ${KERNEL_ROOTDIR}
   make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+  make menuconfig O=out
   make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
