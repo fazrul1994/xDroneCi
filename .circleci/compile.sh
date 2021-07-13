d4e2fbffbac2faf3afefed65c0374e9cb97ff418
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-07-13 09:33:27 +0700

    Update compile.sh

--- .circleci/compile.sh
+++ .circleci/compile.sh
@@ -48,8 +48,13 @@ function compile() {
 
   cd ${KERNEL_ROOTDIR}
   make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-  make -j$(nproc --all) ARCH=arm64 O=out \
-	CC=${CLANG_ROOTDIR}/bin/clang \
+  make -j$(nproc) ARCH=arm64 O=out \
+  	CC=${CLANG_ROOTDIR}/bin/clang \
+	AR=${CLANG_ROOTDIR}/bin/llvm-ar \
+	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
+	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
+	STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
 	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
@@ -65,13 +70,13 @@ function compile() {
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAECfcRg2RoccdYCRdKV9VvHTsGGzPfAGwACSwYAAio_yVZ1PSnOxIKyfSAE-aN927wS5blhsE" \
+        -d sticker="CAACAgUAAxkBAAECjDRg5d7dDFZ6ote8tXFHlM6qMF-YBQACYAIAAg3z6VXA2X_VuGq9KSAE" \
         -d chat_id="${CHAT_ID}"
 }
 
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAECfcRg2RoccdYCRdKV9VvHTsGGzPfAGwACSwYAAio_yVZ1PSnOxIKyfSAE-aN927wS5blhsE" \
+        -d sticker="CAACAgUAAxkBAAECjDRg5d7dDFZ6ote8tXFHlM6qMF-YBQACYAIAAg3z6VXA2X_VuGq9KSAE" \
         -d chat_id="-1001461733416"
 }
 
