12fe09977853377e95a090ce2bcfe52fcbf9153e
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-07-13 08:10:51 +0700

    Create compile.sh

---
+++ .circleci/compile.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+echo "Downloading few Dependecies . . ."
+git clone --depth=1 https://github.com/fazrul1994/azure-clang azure
+
+# Main
+KERNEL_ROOTDIR=$(pwd)/poplar # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=ignominiOus-poplaR_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=$(pwd)/azure # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=NoFace # Change with your own name or else.
+export KBUILD_BUILD_HOST=NoName-circleCI # Change with your own hostname.
+IMAGE=$(pwd)/poplar/out/arch/arm64/boot/Image.gz-dtb
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+function check() {
+echo ================================================
+echo xKernelCompiler
+echo version : rev1.5 - gaspoll
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo CLANG_VERSION = $(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo ================================================
+}
+
+# Compiler
+function compile() {
+
+   # Private CI
+   curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="${chat_id}" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
+        -d Build Started on : ${date}
+   # xyzplaygrnd
+   curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="-1001461733416" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG VERSION : <code>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
+
+  cd ${KERNEL_ROOTDIR}
+  make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+  make -j$(nproc --all) ARCH=arm64 O=out \
+	CC=${CLANG_ROOTDIR}/bin/clang \
+	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+
+   if ! [ -a "$IMAGE" ]; then
+	finerr
+	exit 1
+   fi
+        git clone --depth=1 https://github.com/fazrul1994/AnyKernel3 AnyKernel
+	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+
+
+# sticker plox
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot${token}/sendSticker" \
+        -d sticker="CAACAgUAAxkBAAECfcRg2RoccdYCRdKV9VvHTsGGzPfAGwACSwYAAio_yVZ1PSnOxIKyfSAE-aN927wS5blhsE" \
+        -d chat_id="${chat_id}"
+}
+
+function sticker() {
+    curl -s -X POST "https://api.telegram.org/bot${token}/sendSticker" \
+        -d sticker="CAACAgUAAxkBAAECfcRg2RoccdYCRdKV9VvHTsGGzPfAGwACSwYAAio_yVZ1PSnOxIKyfSAE-aN927wS5blhsE" \
+        -d chat_id="-1001461733416"
+}
+
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot${token}/sendDocument" \
+        -F chat_id="${chat_id}" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Sony Xperia Xz1 (poplar)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+
+    curl -F document=@$ZIP "https://api.telegram.org/bot${token}/sendDocument" \
+        -F chat_id="-1001461733416" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Sony Xperia Xz1 (poplar)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="${chat_id}" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+
+    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
+        -d chat_id="-1001461733416" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+
+    exit 1
+}
+
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 ignominiOus-poplar-${DATE}.zip *
+    cd ..
+}
+check
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
