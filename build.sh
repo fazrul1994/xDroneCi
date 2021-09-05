2b747e86141657e7892f294861b9b7a6f5141937
Akhyar2019 <87224221+Akhyar2019@users.noreply.github.com>
2021-09-05 15:17:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 echo "Downloading few Dependecies . . ."
 git config --global user.name "akhyar2019"
 git config --global user.email "babyakhyar@gmail.com"
+neofetch
 git clone --depth=1 https://github.com/Akhyar2019/BrainDrill-Clang braindrill
-git clone --depth=1 https://github.com/fazrul1994/ignominiOus_Poplar -b test poplar
+git clone --depth=1 https://github.com/fazrul1994/ignominiOus_Poplar poplar
 
 # Main
 KERNEL_ROOTDIR=$(pwd)/poplar # IMPORTANT ! Fill with your kernel source root directory.
