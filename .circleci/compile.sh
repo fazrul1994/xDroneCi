046f12a11b14108ada92119d14e97b38e8935c1a
fazrul1994 <fazrulfadhilah@gmail.com>
2021-10-14 02:00:43 +0700

    build tc circleci

--- .circleci/compile.sh
+++ .circleci/compile.sh
@@ -1,130 +1,114 @@
 #!/usr/bin/env bash
-echo "Downloading few Dependecies . . ."
-git config --global user.name "fazrul1994"
-git config --global user.email "fazrulfadhilah@gmail.com"
-git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang xRage
-git clone --depth=1 https://github.com/fazrul1994/ignominiOus_Poplar poplar
 
-# Main
-KERNEL_ROOTDIR=$(pwd)/poplar # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG=ignominiOus-poplaR_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=$(pwd)/xRage # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER=NoFace # Change with your own name or else.
-export KBUILD_BUILD_HOST=NoName-circleCI # Change with your own hostname.
-IMAGE=$(pwd)/poplar/out/arch/arm64/boot/Image.gz-dtb
-DATE=$(date +"%F-%S")
-START=$(date +"%s")
+# Secret Variable for CI
+# LLVM_NAME | Your desired Toolchain Name
+# TG_TOKEN | Your Telegram Bot Token
+# TG_CHAT_ID | Your Telegram Channel / Group Chat ID
+# GH_USERNAME | Your Github Username
+# GH_EMAIL | Your Github Email
+# GH_TOKEN | Your Github Token ( repo & repo_hook )
+# GH_PUSH_REPO_URL | Your Repository for store compiled Toolchain ( without https:// or www. ) ex. github.com/xyz-prjkt/xRageTC.git
 
-# Checking environtment
-# Warning !! Dont Change anything there without known reason.
-function check() {
-echo ================================================
-echo xKernelCompiler
-echo version : rev1.5 - gaspoll
-echo ================================================
-echo BUILDER NAME = ${KBUILD_BUILD_USER}
-echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
-echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
-echo CLANG_VERSION = $(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
-echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
-echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
-echo ================================================
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
 }
 
-# Compiler
-function compile() {
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
 
-   # Private CI
-   curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
-        -d chat_id="${CHAT_ID}" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
-        -d Build Started on : ${date}
-   # xyzplaygrnd
-   curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
-        -d chat_id="-1001461733416" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG VERSION : <code>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
+# Set a directory
+DIR="$(pwd ...)"
 
-  cd ${KERNEL_ROOTDIR}
-  make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-  make -j36 ARCH=arm64 O=out \
-  	CC=${CLANG_ROOTDIR}/bin/clang \
-	AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-	STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+# Inlined function to post a message
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
 
-   if ! [ -a "$IMAGE" ]; then
-	finerr
+}
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+	-F chat_id="$TG_CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"
+}
+
+# Build Info
+rel_date="$(date "+%Y%m%d")" # ISO 8601 format
+rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
+builder_commit="$(git rev-parse HEAD)"
+
+# Send a notificaton to TG
+tg_post_msg "<b>$LLVM_NAME: Toolchain Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
+
+# Build LLVM
+msg "$LLVM_NAME: Building LLVM..."
+tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
+./build-llvm.py \
+	--clang-vendor "$LLVM_NAME" \
+	--projects "clang;lld;polly" \
+	--targets "ARM;AArch64" \
+	--lto thin \
+	--shallow-clone \
+	--incremental \
+	--build-type "Release" 2>&1 | tee build.log
+
+# Check if the final clang binary exists or not.
+[ ! -f install/bin/clang-1* ] && {
+	err "Building LLVM failed ! Kindly check errors !!"
+	tg_post_build "build.log" "$TG_CHAT_ID" "Error Log"
 	exit 1
-   fi
-        git clone --depth=1 https://github.com/fazrul1994/AnyKernel3 AnyKernel
-	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 
+# Remove unused products
+rm -fr install/include
+rm -f install/lib/*.a install/lib/*.la
 
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAECyulhIzMSBNW-9ih_efmVdvpGMndPhgACHQIAAo_LJwW78W2ICybhxiAE" \
-        -d chat_id="${CHAT_ID}"
-}
+# Strip remaining products
+for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
+	strip -s "${f: : -1}"
+done
 
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAECyulhIzMSBNW-9ih_efmVdvpGMndPhgACHQIAAo_LJwW78W2ICybhxiAE" \
-        -d chat_id="-1001461733416"
-}
+# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
+for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
+	# Remove last character from file output (':')
+	bin="${bin: : -1}"
 
-# Push kernel to channel
-function push() {
-    cd AnyKernel
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot${TOKEN}/sendDocument" \
-        -F chat_id="${CHAT_ID}" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Sony Xperia Xz1 (poplar)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+	echo "$bin"
+	patchelf --set-rpath "$DIR/install/lib" "$bin"
+done
 
-    curl -F document=@$ZIP "https://api.telegram.org/bot${TOKEN}/sendDocument" \
-        -F chat_id="-1001461733416" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Sony Xperia Xz1 (poplar)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+# Release Info
+pushd llvm-project || exit
+llvm_commit="$(git rev-parse HEAD)"
+short_llvm_commit="$(cut -c-8 <<< "$llvm_commit")"
+popd || exit
 
-}
-# Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
-        -d chat_id="${CHAT_ID}" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build throw an error(s)"
+llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
+clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
-    curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
-        -d chat_id="-1001461733416" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build throw an error(s)"
+tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Version : </b><code>$clang_version</code>%0A<b>LLVM Commit : </b><code>$llvm_commit_url</code>%0A<b>Binutils Version : </b><code>$binutils_ver</code>"
 
-    exit 1
-}
+# Push to GitHub
+# Update Git repository
+git config --global user.name $GH_USERNAME
+git config --global user.email $GH_EMAIL
+git clone "https://$GH_USERNAME:$GH_TOKEN@$GH_PUSH_REPO_URL" rel_repo -b lto
+pushd rel_repo || exit
+rm -fr ./*
+cp -r ../install/* .
+git checkout README.md # keep this as it's not part of the toolchain itself
+git add .
+git commit -asm "$LLVM_NAME: Bump to $rel_date build
 
-# Zipping
-function zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 ignominiOus-poplar-${DATE}.zip *
-    cd ..
-}
-check
-compile
-zipping
-END=$(date +"%s")
-DIFF=$(($END - $START))
-push
+LLVM commit: $llvm_commit_url
+Clang Version: $clang_version
+Builder commit: https://$GH_PUSH_REPO_URL/commit/$builder_commit"
+git push -f
+popd || exit
+tg_post_msg "<b>$LLVM_NAME: Toolchain pushed to <code>https://$GH_PUSH_REPO_URL</code></b>"
